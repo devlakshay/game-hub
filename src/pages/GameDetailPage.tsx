d788c07bcee67a6a2547f02d9e4f730b140e4a61
Lakshya Goel <8059800706lakshya@gmail.com>
2024-06-11 15:36:25 +0530

    Fix the navbar

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,13 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttribute from "../components/GameAttribute";
 import CriticScore from "../components/CriticScore";
@@ -15,32 +22,36 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText limit={300}>{game.description_raw}</ExpandableText>
-      <SimpleGrid columns={2} as={"dl"}>
-        <GameAttribute term="Platforms">
-          {game.parent_platforms?.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </GameAttribute>
-        <GameAttribute term="Metascore">
-          <CriticScore score={game.metacritic} />
-        </GameAttribute>
-        <GameAttribute term="Genres">
-          {game.genres?.map((genre) => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </GameAttribute>
-        <GameAttribute term="Publishers">
-          {game.publishers?.map((publisher) => (
-            <Text key={publisher.id}>{publisher.name}</Text>
-          ))}
-        </GameAttribute>
-      </SimpleGrid>
-      <GameTrailer slug={game.slug} />
-      <GameScreenshots id={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText limit={300}>{game.description_raw}</ExpandableText>
+        <SimpleGrid columns={2} as={"dl"}>
+          <GameAttribute term="Platforms">
+            {game.parent_platforms?.map(({ platform }) => (
+              <Text key={platform.id}>{platform.name}</Text>
+            ))}
+          </GameAttribute>
+          <GameAttribute term="Metascore">
+            <CriticScore score={game.metacritic} />
+          </GameAttribute>
+          <GameAttribute term="Genres">
+            {game.genres?.map((genre) => (
+              <Text key={genre.id}>{genre.name}</Text>
+            ))}
+          </GameAttribute>
+          <GameAttribute term="Publishers">
+            {game.publishers?.map((publisher) => (
+              <Text key={publisher.id}>{publisher.name}</Text>
+            ))}
+          </GameAttribute>
+        </SimpleGrid>
+      </GridItem>
+      <GridItem>
+        <GameTrailer slug={game.slug} />
+        <GameScreenshots id={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
